aa502626ca8fa228aa443e18408877ee0a916964
Tawonga021 <reginaldjongwe@gmail.com>
2025-09-24 18:20:11 +0200

    Update logout.js

--- src/redux/actions/logout.js
+++ src/redux/actions/logout.js
@@ -5,19 +5,19 @@ export const logout = (role) => (dispatch) => {
         localStorage.clear('devInfo');
 
         dispatch({ type: LOGOUT })
-        window.location.href="http://localhost:3000/developer-signin" 
+        window.location.href="http://72.60.95.212/developer-signin" 
     }else if (role==="manager") {
         localStorage.clear('managerInfo');
         dispatch({ type: LOGOUT })
-        window.location.href="http://localhost:3000/manager-signin" 
+        window.location.href="http://72.60.95.212/manager-signin" 
     }else if(role==="admin"){
         localStorage.clear('adminInfo');
         dispatch({ type: LOGOUT })
-        window.location.href="http://localhost:3000/admin-signin" 
+        window.location.href="http://72.60.95.212/admin-signin" 
     }else{
         localStorage.clear('cashierInfo');
         dispatch({ type: LOGOUT })
-        window.location.href="http://localhost:3000/cashier-signin" 
+        window.location.href="http://72.60.95.212/cashier-signin" 
     }
  
   }
